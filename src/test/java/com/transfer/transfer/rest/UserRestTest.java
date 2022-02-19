1f5e71958ebf4b2919c09b29fc3b049bbc3200fd
Gruszczynskik96 <gruszczynskikdev@gmail.com>
2022-02-19 16:50:43 +0100

    Minor Refactor Removed redundant imports

--- src/test/java/com/transfer/transfer/rest/UserRestTest.java
+++ src/test/java/com/transfer/transfer/rest/UserRestTest.java
@@ -1,6 +1,5 @@
 package com.transfer.transfer.rest;
 
-import com.transfer.transfer.TransferApplication;
 import com.transfer.transfer.user.model.UserModel;
 import com.transfer.transfer.user.rest.UserRestController;
 import com.transfer.transfer.user.service.UserService;
@@ -9,9 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
